b3ae88316f1c0714680f62043147e7cb224affdd
ackushiw <ackw.kushi@gmail.com>
2019-08-31 02:00:55 -0400

    Split out head into seperate component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
-import Head from 'next/head'
+import Head from '../components/Head'
 import CharacterCard from '../components/CharacterCard'
 
 const Home = () => (
   <div>
-    <Head>
-      <title>Star Wars Attck of the Code</title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-      <link rel="manifest" href="/static/manifest.json" />
-      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#000000" />
-      <meta name="msapplication-TileColor" content="#2b5797" />
-      <meta name="theme-color" content="#ffffff" />
-    </Head>
+    <Head title="Star Wars Attck of the Code" />
   </div>
 )
 
